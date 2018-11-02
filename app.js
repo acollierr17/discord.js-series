30c0cc756c0bee565839da032d2bba0ff2f1e7aa
acollierr17 <gamemaniac101@gmail.com>
2018-11-02 17:26:00 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login('TOKEN_REMOVED');
