f6ad534f078cc1a5646cd2d36799136618227d6f
acollierr17 <gamemaniac101@gmail.com>
2018-11-02 17:40:24 -0400

    Initial commit

--- app.js
+++ app.js
@@ -11,14 +11,45 @@ const config = {
     prefix: process.env.PREFIX
 };
 
+const prefix = config.prefix;
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+// [!!mute @User 12h Posting too many good memes]
+// 0        1     2      3     5   6    7   8   
+// !!mute <user> <time> <reason> \
+
+client.on('message', message => {
+    if (message.author.bot) return;
+    if (message.content.indexOf(prefix) !== 0) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    switch (command) {
+        case 'ping': {
+            message.channel.send('Pong!').catch(console.error);
+            break;
+        }
+        case 'myname': {
+            const name = message.member.displayName;
+            message.delete();
+            message.channel.send(`Your name is ${name}.`);
+            break;
+        }
+        case 'say': {
+            // !!say My name is Jeff
+            const response = args.join(' ');
+            message.delete();
+            message.channel.send(response);
+            break;
+        }
+        default:
+            message.channel.send('This command is unknown!');
+            break;
+    }
 });
 
 client.login(config.token);
