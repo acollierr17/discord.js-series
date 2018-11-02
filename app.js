875855d1c3c9650d83cc23c0cffc31a5808253d2
acollierr17 <gamemaniac101@gmail.com>
2018-11-02 17:33:23 -0400

    Initial commit

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+require('dotenv-flow').config();
+
+// I kept the config.json file even though I will be using .env for the rest of the series
+// only so you can see and know the differnce.
+
+const config = {
+    token: process.env.TOKEN,
+    owner: process.env.OWNER,
+    prefix: process.env.PREFIX
+};
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -11,4 +21,4 @@ client.on('message', msg => {
   }
 });
 
-client.login('TOKEN_REMOVED');
+client.login(config.token);
