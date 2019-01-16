4e0e2821bb8c3c6886d15c4ff6ec6522fed27074
acollierr17 <gamemaniac101@gmail.com>
2019-01-16 18:53:50 -0500

    Initial commit

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,7 +1,8 @@
 module.exports = async (client, messageReaction, user) => {
 
     const message = messageReaction.message;
-    const channel = message.guild.channels.find(c => c.name === 'welcome');
+    const welcomeChannel = message.guild.channels.find(c => c.name === 'welcome');
+    const verifyChannel = message.guild.channels.find(c => c.name === 'verify');
     const member = message.guild.members.get(user.id);
     if (member.user.bot) return;
 
@@ -9,8 +10,16 @@ module.exports = async (client, messageReaction, user) => {
     const b = message.guild.roles.get('485987998165499914'); // Administrator
     const c = message.guild.roles.get('482192667766423561'); // Developer
 
+    const verify = message.guild.roles.get('534445184830930944'); // Verified
+
+    // Verify a member once they have reacted to the message in the verify channel (gives them the Verified role)
+    if (messageReaction.emoji.name === '✅' && message.channel.id === verifyChannel.id) {
+        member.addRole(verify).catch(console.error);
+        return messageReaction.remove(member).catch(console.error);
+    }
+
     // Adds/removes a user from a joinable role via the welcome
-    if (['🇦', '🇧', '🇨'].includes(messageReaction.emoji.name) && message.channel.id === channel.id) {
+    if (['🇦', '🇧', '🇨'].includes(messageReaction.emoji.name) && message.channel.id === welcomeChannel.id) {
         switch (messageReaction.emoji.name) {
             case '🇦':
                 member.addRole(a).catch(console.error);
@@ -24,5 +33,6 @@ module.exports = async (client, messageReaction, user) => {
             default:
                 break;
         }
+        return;
     }
 };
--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,10 @@
 module.exports = client => {
     console.log(`Logged in as ${client.user.tag}!`);
     const welcome = client.channels.find(c => c.name === 'welcome');
-	welcome.fetchMessages({ limit: 10 }).then(collected => console.log('Fetched ' + collected.size + ' messages.')).catch(console.error);
+    const verify = client.channels.find(c => c.name === 'verify');
+
+    const fetchedMessages = [welcome, verify];
+    fetchedMessages.forEach(c => {
+        c.fetchMessages({ limit: 10 }).then(collected => console.log(`Fetched ${collected.size} messages in ${c.name}.`)).catch(console.error);
+    });
 };
